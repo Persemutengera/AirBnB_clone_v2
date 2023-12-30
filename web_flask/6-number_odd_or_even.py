79358a45e78e47690e67b54a19f1ef0ddeb8597f
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-12-30 21:31:23 +0200

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,51 +1,55 @@
 #!/usr/bin/python3
-""" Import flask class """
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
-    """ Function to handle request """
+def hello_hbnb():
+    """ Print Web """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hello_hbnb():
-    """ Function to handle request to path /hbnb """
+def hbnb():
+    """ Print Web """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_route(text):
-    """ Function to handle request with a variable """
-    return 'C %s' % text.replace('_', ' ')
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/')
+@app.route('/python')
 @app.route('/python/<text>')
-def python(text='is cool'):
-    """ Function to handle request with a variable and data default """
-    return 'Python %s' % text.replace('_', ' ')
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool """
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:num>')
-def numbers(num):
-    """ Function to handle request with a variable with specified type """
-    return '%d is a number' % num
+@app.route('/number/<int:n>')
+def number(n):
+    """ Number route """
+    return '{} is number'.format(n)
 
 
-@app.route('/number_template/<int:num>')
-def numbers_temp(num):
-    """ Function to handle request and render template"""
-    return render_template('5-number.html', number=num)
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Display a HTML page only if n is an integer """
+    return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd_or_even/<int:num>')
-def odd_or_even(num):
-    """ Function to handle request and render template"""
-    return render_template('6-number_odd_or_even.html', number=num)
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """ Show if the number is even or odd """
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
