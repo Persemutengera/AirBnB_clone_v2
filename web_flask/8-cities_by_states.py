9e6f01a83671b34b1cf88e444100a4dfae50244c
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-12-30 21:41:11 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,55 @@
 #!/usr/bin/python3
-""" Import flask class """
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
-from models import storage, State
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """ Function to handle request """
-    states = storage.all(State)
-    dict_states = {}
-    for _, value in states.items():
-        dict_states[value.id] = value.name
-    return render_template('7-states_list.html', states=dict_states)
+@app.route('/')
+def hello_hbnb():
+    """ Print Web """
+    return 'Hello HBNB!'
 
 
-@app.route('/cities_by_states')
-def cities_states():
-    """ Function to handle request cities"""
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+@app.route('/hbnb')
+def hbnb():
+    """ Print Web """
+    return 'HBNB'
 
 
-@app.teardown_appcontext
-def storage_close(exception=None):
-    storage.close()
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """ Number route """
+    return '{} is number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Display a HTML page only if n is an integer """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """ Show if the number is even or odd """
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
