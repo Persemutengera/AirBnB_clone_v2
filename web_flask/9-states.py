32c4e069a1ae444b4a8c1c669b3c51ed05561933
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-12-30 10:29:12 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
-""" Import flask class """
-from flask import Flask, render_template
-from models import storage, State
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """ Function to handle request """
-    states = storage.all(State)
-    dict_states = {}
-    for _, value in states.items():
-        dict_states[value.id] = value.name
-    return render_template('7-states_list.html', states=dict_states)
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 
-@app.route('/cities_by_states')
-def cities_states():
-    """ Function to handle request cities"""
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
-@app.route('/states/')
-@app.route('/states/<id>')
-def states_by_id(id=None):
-    """ Function to handle request cities"""
-    states = storage.all(State)
-    if id is not None:
-        class_id = "State." + id
-        if class_id in states:
-            return render_template('9-states.html', state=states[class_id])
-        else:
-            return render_template('9-states.html', state=None)
-    dict_states = {}
-    for _, value in states.items():
-        dict_states[value.id] = value.name
-    return render_template('7-states_list.html', states=dict_states)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def storage_close(exception=None):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
